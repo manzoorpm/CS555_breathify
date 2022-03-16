c2d4c8259fde3ba1ff2490d793ed30d31e8d3d35
manzoorpm <manzoorpm@live.com>
2022-03-16 01:57:02 -0400

    Login fix + Home page refirect

--- breathify/appone/urls.py
+++ breathify/appone/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.signin, name="signin"),
+    path('', views.home, name="home"),
+    path('signin/', views.signin, name="signin"),
+    path('signout/', views.signout, name="signout"),
+    path('registration/', views.registration, name="registration"),
 
 ]
--- breathify/appone/views.py
+++ breathify/appone/views.py
@@ -1,9 +1,52 @@
-from http.server import ThreadingHTTPServer
-from itertools import product
 from django.shortcuts import render, redirect 
 from django.http import HttpResponse
 from .models import *
+from .forms import userform
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
 
 def signin(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'appone/signin.html', context)
+
+def signout(request):
+	logout(request)
+	return redirect('signin')
+
+
+def registration(request):
 	
-	return render(request, 'appone/sign_in.html')
+	form = userform()
+
+	if request.method == 'POST':
+		form = userform(request.POST)
+		if form.is_valid():
+			form.save()
+			user = form.cleaned_data.get('username')
+			messages.success(request, 'Account was created for' + user)
+			return redirect('signin')
+
+	context = {'form': form}
+	return render(request, 'appone/registration.html', context)
+
+@login_required(login_url='signin')
+def home(request):
+	context = {}
+	return render(request, 'appone/home.html')
